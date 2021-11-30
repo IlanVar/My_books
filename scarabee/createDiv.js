7698e9ef26c7eeefb48f9a2b919dd634f396ac58
IlanVar <86364884+IlanVar@users.noreply.github.com>
2021-11-30 16:42:34 +0100

    Add files via upload

---
+++ scarabee/createDiv.js
@@ -0,0 +1,9 @@
+const numOfPapers = Number(10);
+
+function createDiv(){
+    for (let index = 1; index <= numOfPapers; index++) {
+        document.write("<div id='p"+index+"' class='paper' style='z-index:"+(numOfPapers-index+1)+"'><div class='front'><div id='f"+index+"' class='front-content'><img src='data/pages/"+((index*2)-1)+".jpg'></div></div><div class='back'><div id='b"+index+"' class='back-content'><img src='data/pages/"+(index*2)+".jpg'></div></div></div>");
+    }
+}
+
+createDiv()
